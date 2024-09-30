c46e33ae29c0627f3db99d6362b0478114a1855b
sp89-dev <sourabhpaliwal89@gmail.com>
2024-09-30 14:16:45 +0000

    eureka server config

--- user-service-feign/src/main/java/sp/ms/feign/userservice/APIClient.java
+++ user-service-feign/src/main/java/sp/ms/feign/userservice/APIClient.java
@@ -5,7 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import sp.ms.feign.userservice.DTO.DepartmentDto;
 
-@FeignClient(value = "department-service", url = "http://localhost:8989")
+//for the Feign client, we need to specify the name of the service we want to call - without spring cloud config server
+// @FeignClient(value = "department-service", url = "http://localhost:8989")
+// after spring cloud config server - department service port is 8080 and user service fiegn port is 8081
+// @FeignClient(value = "department-service", url = "http://localhost:8080")// config server
+@FeignClient(value = "department-service", url = "http://DEPARTMENT-SERVICE") // eureka server
 public interface APIClient {
     @GetMapping("/api/departments/{id}")
     DepartmentDto findDepartmentById(@PathVariable("id") Long departmentId);
